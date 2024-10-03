131b3258efa932b07aa1074bab55f15537dea30d
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-10-02 20:03:51 -0600

    remove spaces

--- mov1.asm
+++ mov1.asm
@@ -1,15 +1,20 @@
-.model small ;se asigna formato de memoria
-.stack ;modelo de pila
-.data ;inicio de datos
-mensaje db "Tecnologico de Estudios Superiores de Jilotepec","$" ;mensaje
-.code ;inicio de codigo
-main proc ;inicia proceso principal
-mov ax, SEG @data ;se asigna la localizacion del segmento de datos
-mov ds,ax ;coloca los datos contenidos en ax en el segmento de datos
-mov ah,09 ;se imprime la cadena
-lea dx,mensaje ;leer mensaje
-int 21h ;funcion de interrupcion que invoca al DOS
-mov ax,4c00h ;salir del programa
-int 21h
-main endp ;termina procedimiento
+.model small    ;se asigna formato de memoria
+.stack  ;modelo de pila
+
+.data   ;inicio de datos
+
+mensaje db "Tecnologico de Estudios Superiores de Jilotepec","$"    ;mensaje
+
+.code   ;inicio de codigo
+
+main proc   ;inicia proceso principal
+    mov ax, SEG @data   ;se asigna la localizacion del segmento de datos
+    mov ds,ax   ;coloca los datos contenidos en ax en el segmento de datos
+    mov ah,09   ;se imprime la cadena
+    lea dx,mensaje  ;leer mensaje
+    int 21h     ;funcion de interrupcion que invoca al DOS
+    mov ax,4c00h    ;salir del programa
+    int 21h
+main endp  ;termina procedimiento
+
 end main
