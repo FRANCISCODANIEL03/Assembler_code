95cc1ab5a16b0282b51a366a4af9b0228fcb13bf
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2024-10-27 17:29:43 -0600

    add function inicio

--- msg.asm
+++ msg.asm
@@ -5,3 +5,18 @@
     msg db "mensaje$"
 .code
 
+inicio:
+    mov ax,@data
+    mov ds,ax
+    mov ah,09h
+    mov dx, offset msg
+    int 21h
+    mov ah,02
+    mov dl,0ah
+    int 21h
+ 
+    mov ah,02
+    mov dl,0dh
+    int 21h
+ 
+    mov SI,0
