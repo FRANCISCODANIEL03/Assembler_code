1fe3db447fa8fe46f1fe6d119e905d20265d0bee
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2024-11-04 21:15:06 -0600

    align instructions

--- meth.asm
+++ meth.asm
@@ -3,22 +3,22 @@
 
 .data  ; Segmento de datos
 
-numero1 db 0
-numero2 db 0
-suma db 0
-resta db 0
-mult db 0
-divi db 0
-modu db 0
+    numero1 db 0
+    numero2 db 0
+    suma db 0
+    resta db 0
+    mult db 0
+    divi db 0
+    modu db 0
 
-mensaje1 db 10, 13, "Ingresa el primer numero (0-99): $"
-mensaje2 db 10, 13, "Ingresa el segundo numero (0-99): $"
-mensaje3 db 10, 13, "La suma es: $"
-mensaje4 db 10, 13, "La resta es: $"
-mensaje5 db 10, 13, "La multiplicacion es: $"
-mensaje6 db 10, 13, "La division es: $"
-mensaje7 db 10, 13, "El modulo es: $"
-saltoLinea db 10, 13, "$"
+    mensaje1 db 10, 13, "Ingresa el primer numero (0-99): $"
+    mensaje2 db 10, 13, "Ingresa el segundo numero (0-99): $"
+    mensaje3 db 10, 13, "La suma es: $"
+    mensaje4 db 10, 13, "La resta es: $"
+    mensaje5 db 10, 13, "La multiplicacion es: $"
+    mensaje6 db 10, 13, "La division es: $"
+    mensaje7 db 10, 13, "El modulo es: $"
+    saltoLinea db 10, 13, "$"
 
 .code  ; Segmento de codigo
 main proc
