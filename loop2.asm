e50e53a1557e550e58a7eff64c6078b27919215a
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2024-11-04 21:24:35 -0600

    align instructions

--- loop2.asm
+++ loop2.asm
@@ -3,9 +3,9 @@
 
 .data   ;inicio de datos
 
-mensaje db "Tecnologico de Estudios Superiores de Jilotepec","$"    ;mensaje
-mensaje2 db 10,13,4,"Ingenieria en sistemas computacionales","$"
-mensaje3 db 10,13,"                                          ",10,13,"$"
+    mensaje db "Tecnologico de Estudios Superiores de Jilotepec","$"    ;mensaje
+    mensaje2 db 10,13,4,"Ingenieria en sistemas computacionales","$"
+    mensaje3 db 10,13,"                                          ",10,13,"$"
 
 .code   ;inicio de codigo
 
@@ -14,16 +14,16 @@ main proc   ;inicia proceso principal
     main proc
     mov cx,5
     ciclo:
-    mov ax, SEG @data   ;se asigna la localizacion del segmento de datos
-    mov ds,ax   ;coloca los datos contenidos en ax en el segmento de datos
-    mov ah,09   ;se imprime la cadena
-    lea dx,mensaje  ;leer mensaje
-    int 21h
-    lea dx,mensaje2 
-    int 21h     ;funcion de interrupcion que invoca al DOS
-    mov ah,09
-    lea dx,mensaje3
-    int 21h
+        mov ax, SEG @data   ;se asigna la localizacion del segmento de datos
+        mov ds,ax   ;coloca los datos contenidos en ax en el segmento de datos
+        mov ah,09   ;se imprime la cadena
+        lea dx,mensaje  ;leer mensaje
+        int 21h
+        lea dx,mensaje2 
+        int 21h     ;funcion de interrupcion que invoca al DOS
+        mov ah,09
+        lea dx,mensaje3
+        int 21h
     
     loop ciclo
     
