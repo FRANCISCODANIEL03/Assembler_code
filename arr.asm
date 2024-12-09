90370381ff901eca8ee58895142d9ce1745042f6
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2024-12-09 02:03:31 -0600

    create main method

--- arr.asm
+++ arr.asm
@@ -10,3 +10,29 @@ arreglo db 1,2,3,4
 cadena db "ISIC  $"
 var db 7
 .code
+main proc
+mov ax, seg @data
+mov ds,ax
+
+mov ah,09h
+lea dx,cadena
+int 21h
+
+mov al,arreglo[0]
+mov bl,arreglo[1]
+mov cl,arreglo[2]
+
+mov ah,02
+add al,30h
+mov dl,al
+int 21h
+
+mov ah,02
+add bl,30h
+mov dl,bl
+int 21h
+
+mov ah,02
+add cl,30h
+mov dl,cl
+int 21h
