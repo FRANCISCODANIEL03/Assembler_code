0bf8d73f8279d7ec2cf3878cf4a45197f35f3aac
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2024-12-13 02:10:14 -0600

    add main method

--- arre.asm
+++ arre.asm
@@ -7,3 +7,41 @@
     msg db "Elementos del arreglo: $"
     coma db ", $"
 .code
+
+main proc
+    mov ax,@data
+    mov ds,ax
+    
+    mov ah,09h
+    lea dx,msg
+    int 21h
+    
+    mov al,arreglo[0]
+    add al,30h
+    mov dl,al
+    mov ah,02h
+    int 21h
+        
+    mov ah,09h
+    lea dx,coma
+    int 21h
+    
+    mov bl,arreglo[1]
+    add bl,30h
+    mov dl,bl
+    mov ah,02h
+    int 21h
+    
+    mov ah,09h
+    lea dx,coma
+    int 21h
+    
+    mov cl,arreglo[2]
+    add cl,30h
+    mov dl,cl
+    mov ah,02h
+    int 21h
+    
+    mov ah,4ch
+    mov al,00h
+    int 21h
